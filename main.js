4a68689d5eb2434ed5640c7075df155248d494bf
Rita <rita.anierobi@stu.ocsb.ca>
2024-10-04 15:02:06 +0000

    1

--- main.js
+++ main.js
@@ -8,3 +8,17 @@
 
 // Feel free to delete this line when starting the task
 console.log("Hello world!")
+
+console.log("Apple Inc.\nOne Apple Park Way\nCupertino, CA\n95014\n1 408-996-1010")
+
+console.log("\n\n")
+
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080")
+
+console.log("\n\n")
+
+console.log("x\to\tx")
+console.log("o\tx\to")
+console.log("x\to\tx")
+
+
